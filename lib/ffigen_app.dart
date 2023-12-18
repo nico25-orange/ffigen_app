f8e11aeee36eecd848c71999370fb43a3f846b54
nico25-orange <masa25hw@gmail.com>
2023-12-18 23:44:37 +0900

    feat: Implement the Read Eval Print Loop

--- lib/ffigen_app.dart
+++ lib/ffigen_app.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 import 'dart:io' show Platform;
 import 'package:ffi/ffi.dart' as ffi;
+import 'package:path/path.dart' as p;             // Add this import
 
 import 'duktape_bindings_generated.dart';
 
@@ -8,12 +9,30 @@ const String _libName = 'ffigen_app';
 
 final DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
+    // Add from here...
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return DynamicLibrary.open('build/macos/Build/Products/Debug'
+          '/$_libName/$_libName.framework/$_libName');
+    }
+    // ...to here.
     return DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isAndroid || Platform.isLinux) {
+    // Add from here...
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return DynamicLibrary.open(
+          'build/linux/x64/debug/bundle/lib/lib$_libName.so');
+    }
+    // ...to here.
     return DynamicLibrary.open('lib$_libName.so');
   }
   if (Platform.isWindows) {
+    // Add from here...
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return DynamicLibrary.open(p.canonicalize(
+          p.join(r'build\windows\runner\Debug', '$_libName.dll')));
+    }
+    // ...to here.
     return DynamicLibrary.open('$_libName.dll');
   }
   throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
@@ -27,9 +46,10 @@ class Duktape {
         _bindings.duk_create_heap(nullptr, nullptr, nullptr, nullptr, nullptr);
   }
 
-  void evalString(String jsCode) {
+  // Modify this function
+  String evalString(String jsCode) {
     var nativeUtf8 = jsCode.toNativeUtf8();
-    _bindings.duk_eval_raw(
+    final evalResult = _bindings.duk_eval_raw(
         ctx,
         nativeUtf8.cast<Char>(),
         0,
@@ -40,10 +60,22 @@ class Duktape {
             DUK_COMPILE_STRLEN |
             DUK_COMPILE_NOFILENAME);
     ffi.malloc.free(nativeUtf8);
+
+    if (evalResult != 0) {
+      throw _retrieveTopOfStackAsString();
+    }
+
+    return _retrieveTopOfStackAsString();
   }
 
-  int getInt(int index) {
-    return _bindings.duk_get_int(ctx, index);
+  // Add this function
+  String _retrieveTopOfStackAsString() {
+    Pointer<Size> outLengthPtr = ffi.calloc<Size>();
+    final errorStrPtr = _bindings.duk_safe_to_lstring(ctx, -1, outLengthPtr);
+    final returnVal =
+        errorStrPtr.cast<ffi.Utf8>().toDartString(length: outLengthPtr.value);
+    ffi.calloc.free(outLengthPtr);
+    return returnVal;
   }
 
   void dispose() {
